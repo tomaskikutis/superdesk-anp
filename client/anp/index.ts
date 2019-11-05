cfce537ecdf8730942382014684627e7a5346920
Tomas Kikutis <t.kikutis@gmail.com>
2019-11-05 12:21:24 +0100

    enable planning extension

--- client/anp/index.ts
+++ client/anp/index.ts
@@ -1,4 +1,13 @@
 import photo from './photo';
+import {startApp} from 'superdesk-core/scripts/index';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/src/extension';
+
+setTimeout(() => {
+    startApp(
+        [planningExtension],
+        {},
+    );
+});
 
 export default angular.module('anp', [
     photo.name,
